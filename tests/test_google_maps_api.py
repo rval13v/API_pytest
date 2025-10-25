eea83fd5b6a1c188bdbaafe91109eca16e43f1d7
rval13v <livarfpv@gmail.com>
2025-10-25 21:46:24 +1100

    post

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -9,3 +9,18 @@ class TestCreatePlace():
 
         print('Метод POST')
         result_post: Response = GoogleMapsApi.create_new_place()
+
+        print(f'Статус-код: {result_post.status_code}')
+        assert result_post.status_code == 200
+        print('Стату-код POST корректен')
+
+        # Получаем данные из JSON-ответа.
+        check_response_post = result_post.json()
+
+        # Проверяем, что поле status в ответе имеет значение OK.
+        status = check_response_post.get('status')
+        print(status)
+        assert status == 'OK'
+        print('Поле Status корректно')
+
+
