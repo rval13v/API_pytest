4584850fc43b6a71bb1a2e08f7d623aa424b18f1
rval13v <livarfpv@gmail.com>
2025-10-26 20:52:10 +1100

    get+put

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,12 +1,10 @@
-from requests import  Response
-
+from requests import Response
 from utils.api import GoogleMapsApi
 
 
 class TestCreatePlace():
 
     def test_create_new_place(self):
-
         print('Метод POST')
         result_post: Response = GoogleMapsApi.create_new_place()
 
@@ -24,14 +22,42 @@ class TestCreatePlace():
         print('Поле Status корректно')
 
         place_id = check_response_post.get("place_id")
-        #________GET__________________________________
-        print('\nМетод GET')
+
+        #________GET после POST____________________________________
+        print('\nМетод GET после POST')
         result_get: Response = GoogleMapsApi.get_new_place(place_id)
 
         print(f'Статус-код: {result_get.status_code}')
         assert 200 == result_get.status_code
-        print('Стату-код GET корректен')
+        print('Стату-код GET POST корректен')
 
+        # ________PUT__________________________________
+        print('\nМетод PUT')
+        result_put: Response = GoogleMapsApi.put_new_place(place_id)
 
+        print(f'Статус-код: {result_put.status_code}')
+        assert 200 == result_put.status_code
+        if result_put.status_code == 200:
+            print('Стату-код GET PUT корректен')
+        elif result_put.status_code == 404:
+            print(f'Статус: {result_put.status_code}, Ошибка, локация не найдена')
 
+        check_response_put = result_put.json()
+        msg = check_response_put.get('msg')
+        print(msg)
+        assert msg == 'Address successfully updated'
+        print('Поле MSG корректно')
+
+        # ________GET (после PUT)__________________________________
+        print('\nМетод GET (после PUT)')
+        result_get_after_put: Response = GoogleMapsApi.get_new_place(place_id)
+        print(f'Статус-код: {result_get_after_put.status_code}')
+        assert 200 == result_get_after_put.status_code
+        print('Стату-код GET после PUT корректен')
+
+        # check_response_get_after_put = result_get_after_put.json()
+        # actual_address = check_response_get_after_put.get('address')
+        # print(f"Фактический адрес после обновления: {actual_address}")
+        # assert actual_address == "100 Lenina street, RU"
+        # print('Адрес изменился')
 
