67d286454bd2260f329d3736d5abbb92a878af61
rval13v <livarfpv@gmail.com>
2025-10-28 21:17:09 +1100

    check

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,83 +1,33 @@
-from requests import Response
+from requests import  Response
+from utils.checking import Checking
 from utils.api import GoogleMapsApi
 
 
 class TestCreatePlace:
+
     def test_create_new_place(self):
         print('Метод POST')
         result_post: Response = GoogleMapsApi.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+        Checking.check_status_code(result_post, 200)
 
-        print(f'Статус-код: {result_post.status_code}')
-        assert result_post.status_code == 200
-        print('Стату-код POST корректен')
-
-        # Получаем данные из JSON-ответа.
-        check_response_post = result_post.json()
-
-        # Проверяем, что поле status в ответе имеет значение OK.
-        status = check_response_post.get('status')
-        print(status)
-        assert status == 'OK'
-        print('Поле Status корректно')
-
-        place_id = check_response_post.get("place_id")
-
-        #________GET после POST____________________________________
-        print('\nМетод GET после POST')
+        print('Метод GET POST')
         result_get: Response = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
-        print(f'Статус-код: {result_get.status_code}')
-        assert 200 == result_get.status_code
-        print('Стату-код GET POST корректен')
-
-        # ________PUT__________________________________
-        print('\nМетод PUT')
+        print('Метод PUT')
         result_put: Response = GoogleMapsApi.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
 
-        print(f'Статус-код: {result_put.status_code}')
-        assert 200 == result_put.status_code, f'Статус: {result_put.status_code}, Ошибка, локация не найдена'
-        print('Стату-код GET PUT корректен')
+        print('Метод GET PUT')
+        result_get: Response = GoogleMapsApi.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
 
-        check_response_put = result_put.json()
-        msg = check_response_put.get('msg')
-        print(msg)
-        assert msg == 'Address successfully updated'
-        print('Поле MSG корректно')
-
-        # ________GET (после PUT)__________________________________
-        print('\nМетод GET (после PUT)')
-        result_get_after_put: Response = GoogleMapsApi.get_new_place(place_id)
-        print(f'Статус-код: {result_get_after_put.status_code}')
-        assert 200 == result_get_after_put.status_code
-        print('Стату-код GET после PUT корректен')
-
-        check_response_get_after_put = result_get_after_put.json()
-        actual_address = check_response_get_after_put.get('address')
-        print(f"Фактический адрес после обновления: {actual_address}")
-        assert actual_address == "100 Lenina street, RU"
-        print('Адрес изменился')
-
-        #____DELETE______________________________________________
         print('Метод DELETE')
         result_delete: Response = GoogleMapsApi.delete_new_place(place_id)
-
-        response_json = result_delete.json()
-        print(f"Отправка DELETE-запроса для place_id '{place_id}':")
-        print(f"  Статус-код: {result_delete.status_code}")
-        assert result_delete.status_code == 200
-        print(f"  Статус из ответа: {response_json.get('status')}")
-        assert response_json.get('status') == 'OK'
-        print("  Удаление успешно.")
+        Checking.check_status_code(result_delete, 200)
 
         print('Метод GET DELETE')
         result_get: Response = GoogleMapsApi.get_new_place(place_id)
-
-        print(f'Статус-код: {result_get.status_code}')
-        assert result_get.status_code == 404
-        print('Стутс-код корректен, локация удалена')
-
-        check_response_get = result_get.json()
-        msg = check_response_get.get('msg')
-        print(msg)
-        assert msg == "Get operation failed, looks like place_id  doesn't exists"
-        print('Поле MSG корректно')
+        Checking.check_status_code(result_get, 404)
---
+++ utils/checking.py
@@ -0,0 +1,12 @@
+from requests import Response
+
+
+class Checking:
+
+    @staticmethod
+    def check_status_code(response: Response, status_code):
+        assert status_code == response.status_code
+        if response.status_code == status_code:
+            print("Успешно. Статус код = " + str(response.status_code))
+        else:
+            print("Провал. Статус код = " + str(response.status_code))
