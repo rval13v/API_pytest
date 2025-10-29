9f8287dfe73a5b0ca0d31f749bfa29f59140caa3
rval13v <livarfpv@gmail.com>
2025-10-29 11:46:32 +1100

    check_value

--- utils/checking.py
+++ utils/checking.py
@@ -1,3 +1,4 @@
+import json
 from requests import Response
 
 
@@ -10,3 +11,9 @@ class Checking:
             print("Успешно. Статус код = " + str(response.status_code))
         else:
             print("Провал. Статус код = " + str(response.status_code))
+
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print("Все поля присутсвуют!")
