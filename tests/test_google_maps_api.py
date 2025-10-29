ba28d8faf57b60d09b3854fd2500d8e72ee43e58
rval13v <livarfpv@gmail.com>
2025-10-29 11:45:54 +1100

    check_value

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -11,23 +11,31 @@ class TestCreatePlace:
         check_post = result_post.json()
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
 
         print('Метод GET POST')
         result_get: Response = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
 
         print('Метод PUT')
         result_put: Response = GoogleMapsApi.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
 
         print('Метод GET PUT')
         result_get: Response = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
 
         print('Метод DELETE')
         result_delete: Response = GoogleMapsApi.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
 
         print('Метод GET DELETE')
         result_get: Response = GoogleMapsApi.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+
+        print('Тестирование создания, изменения и удаления новой локации прошло успешно!.')
