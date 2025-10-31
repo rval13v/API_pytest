7b940cc4d235265117316a82c682bbaa7eb723fa
rval13v <livarfpv@gmail.com>
2025-10-31 16:10:34 +1100

    logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,59 +1,59 @@
-import requests  # Импортируем библиотеку requests для выполнения HTTP-запросов.
+# Импортируем модуль datetime для работы с датой и временем.
+import datetime
+# Импортируем модуль os для взаимодействия с операционной системой, например, для получения переменных окружения.
+import os
 
-from utils.logger import Logger
+# Импортируем класс Response из библиотеки requests для аннотаций типов.
+from requests import Response
 
-# Создали класс HTTP_methods, который будет содержать статические методы
-# для отправки различных типов HTTP-запросов (GET, POST, PUT, DELETE).
-class HTTPMethods:
-    # Определяем атрибут класса headers (заголовки), которые будут
-    # использоваться во всех запросах. Content-Type: application/json
-    # указывает, что мы отправляем и ожидаем данные в формате JSON.
-    headers = {'Content-Type': 'application/json'}
 
-    # Определяем атрибут класса cookie для хранения куки.
-    # В данном случае он пуст, но его можно было бы заполнить
-    # для отправки куки с запросами.
-    cookie = ""
+# Создаём класс Logger для ведения логов.
+class Logger:
 
-    # @staticmethod — это декоратор, который объявляет, что следующий метод
-    # является статическим. Это означает, что метод принадлежит классу, а не
-    # конкретному экземпляру класса. Ему не нужен доступ к self или cls.
-    @staticmethod
-    def get(url):
-        # Отправляем GET-запрос по указанному URL.
-        # Используем заголовки и куки, определенные как атрибуты класса.
-        Logger.add_request(url, method="GET")
-        result = requests.get(url, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
-        Logger.add_response(result)
-        # Возвращаем объект ответа (Response object), который содержит
-        # всю информацию о результате запроса.
-        return result
+    # Определяем переменную класса, которая будет хранить имя файла лога.
+    # Имя файла формируется с использованием текущей даты и времени для уникальности.
+    file_name = f"logs/log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
 
-    @staticmethod
-    def post(url, body):
-        Logger.add_request(url, method="POST")
-        # Отправляем POST-запрос по указанному URL.
-        # Параметр json=body автоматически сериализует
-        # словарь body в строку JSON и отправляет её в теле запроса.
-        result = requests.post(url, json=body, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
-        Logger.add_response(result)
-        return result
+    # Определяем метод класса, который записывает данные в файл.
+    @classmethod
+    def write_log_to_file(cls, data: str):
+        # Открываем файл в режиме добавления ('a') с указанием кодировки UTF-8,
+        # чтобы избежать проблем с русскими символами.
+        with open(cls.file_name, 'a', encoding='utf=8') as logger_file:
+            # Записываем данные в файл.
+            logger_file.write(data)
 
-    @staticmethod
-    def put(url, body):
-        Logger.add_request(url, method="PUT")
-        # Отправляем PUT-запрос по указанному URL.
-        # Как и в случае с POST, данные в body сериализуются в JSON.
-        result = requests.put(url, json=body, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
-        Logger.add_response(result)
-        return result
+    # Определяем метод класса для добавления информации о запросе в лог.
+    @classmethod
+    def add_request(cls, url: str, method: str):
+        # Получаем имя текущего теста из переменной окружения.
+        test_name = os.environ.get('PYTEST_CURRENT_TEST')
 
-    @staticmethod
-    def delete(url, body):
-        Logger.add_request(url, method="DELETE")
-        # Отправляем DELETE-запрос по указанному URL.
-        # Хотя DELETE-запросы часто не имеют тела, иногда оно используется,
-        # поэтому здесь предусмотрена возможность его передачи.
-        result = requests.delete(url, json=body, headers=HTTPMethods.headers, cookies=HTTPMethods.cookie)
-        Logger.add_response(result)
-        return result
+        # Формируем строку с информацией о запросе.
+        data_to_add = f"\n-----\n"
+        data_to_add += f"Test: {test_name}\n"
+        data_to_add += f"Time: {str(datetime.datetime.now())}\n"
+        data_to_add += f"Request method: {method}\n"
+        data_to_add += f"Request URL: {url}\n"
+        data_to_add += "\n"
+
+        # Записываем сформированную строку в файл лога.
+        cls.write_log_to_file(data_to_add)
+
+    # Определяем метод класса для добавления информации об ответе в лог.
+    @classmethod
+    def add_response(cls, result: Response):
+        # Преобразуем cookies из объекта ответа в словарь.
+        cookies_as_dict = dict(result.cookies)
+        # Преобразуем заголовки из объекта ответа в словарь.
+        headers_as_dict = dict(result.headers)
+
+        # Формируем строку с информацией об ответе.
+        data_to_add = f"Response code: {result.status_code}\n"
+        data_to_add += f"Response text: {result.text}\n"
+        data_to_add += f"Response headers: {headers_as_dict}\n"
+        data_to_add += f"Response cookies: {cookies_as_dict}\n"
+        data_to_add += f"\n-----\n"
+
+        # Записываем сформированную строку в файл лога.
+        cls.write_log_to_file(data_to_add)
