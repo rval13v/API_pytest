260b4258c5558697f3ab22132b1f5fbd17582559
rval13v <livarfpv@gmail.com>
2025-10-31 16:35:32 +1100

    Update checking.py

--- utils/checking.py
+++ utils/checking.py
@@ -7,9 +7,9 @@ class Checking:
     @staticmethod
     def check_status_code(response: Response, status_code):
         assert status_code == response.status_code, \
-            f"Провал. Статус код = {response.status_code}"
+            f"Провал. Ожидаемый статус код: {status_code}, Фактический: {response.status_code}."
         print("Успешно. Статус код = " + str(response.status_code))
-    
+
     @staticmethod
     def check_json_token(response: Response, expected_value):
         token = json.loads(response.text)
@@ -27,6 +27,6 @@ class Checking:
     def check_json_search_word_in_value(response: Response, field_name, search_word):
         check = response.json()
         check_info = check.get(field_name)
-        assert search_word in check_info:, \
-            f"Провал. Слово '{search_word}' отсутвует в поле '{field_name}'"
+        assert search_word in check_info, \
+            f"Провал. Слово {search_word} отсутствует в поле {field_name}. Фактическое значение: {check_info}"
         print("Слово " + search_word + " присутствует.")
